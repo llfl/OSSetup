4922b9dd9fbe0d103790e90ab69d5f60655630eb
Kun <quinnHugh@hotmail.com>
2019-04-07 14:27:25 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,9 +7,14 @@ if [ $1 = "zsh" or $1 = "all"]; then
 fi
 
 if [ $1 = "anaconda" or $1 = "all"]; then
-    bash ./setup_anaaconda.sh
+    bash ./setup_anaconda.sh
 fi
 
 if [ $1 = "docker" or $1 = "all"]; then
     bash ./setup_docker.sh
+fi
+
+if [ $1 = "tuna" or $1 = "all"]; then
+    wget https://tuna.moe/oh-my-tuna/oh-my-tuna.py
+    python oh-my-tuna.py
 fi
