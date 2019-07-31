41307e3151e30fd51ac211f413236bd714160691
Kun <quinnHugh@hotmail.com>
2019-07-31 11:12:38 +0800

    Create config_zsh.sh

---
+++ config_zsh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+cp ~/.zshrc ~/.zshrc_bak
+
+sed -i '/^plugins/s/)/ zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+
+source ~/.zshrc
