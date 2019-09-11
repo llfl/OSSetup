177ce7e0b04cdfad17015f91a921b1241f9a52b2
Kun <quinnHugh@hotmail.com>
2019-09-11 13:16:55 +0800

    Update setup_zsh.sh

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-sudo apt install -y zsh vim
+sudo apt install -y git zsh vim
 
 sudo chsh -s $(which zsh)
 
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+cp ~/.zshrc ~/.zshrc_bak
+
+sed -i '/^plugins/s/)/ zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+
+sed -i '/^ZSH_THEME/s/"robbyrussell"/"ys"/' ~/.zshrc
+
+source ~/.zshrc
