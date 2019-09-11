48a63ad196f5f648ed356bb95a0d6d39b67997bc
Kun <quinnHugh@hotmail.com>
2019-09-11 15:20:26 +0800

    Update k8sSetup.sh

--- k8sSetup.sh
+++ k8sSetup.sh
@@ -13,9 +13,9 @@ fi
 
 curl -fsSL https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add -
 
-sudo echo "deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main" >>/etc/apt/sources.list.d/kubernetes.list
-
-
+sudo cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
+EOF
 
 sudo apt update && sudo apt install -y kubelet kubeadm kubectl
 
