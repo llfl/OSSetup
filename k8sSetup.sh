ca0cf03d92230c185edd63fea94419c7d3767694
Kun <quinnHugh@hotmail.com>
2019-09-11 14:47:33 +0800

    Update k8sSetup.sh

--- k8sSetup.sh
+++ k8sSetup.sh
@@ -2,7 +2,7 @@
 
 sudo apt update && sudo apt install -y curl
 
-if [ $1 = "docker" or $1 = "d" ]; then
+if [ "$1" = 'docker' or "$1" = 'd' ]; then
     bash ./setup_docker.sh
 fi
 
@@ -11,7 +11,7 @@ sudo echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sou
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 
-if [ $1 = "master" or $1 = "m" ]; then
+if [ "$1" = 'master' or "$1" = 'm' ]; then
     sudo sed -i "s/cgroup-driver=systemd/cgroup-driver=cgroupfs/g" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
     sudo systemctl daemon-reload
     sudo systemctl restart kubelet
