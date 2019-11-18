897f4b7e91be77993c56a5188651b3c5fc900b72
Kun <quinnHugh@hotmail.com>
2019-11-18 19:43:35 +0800

    Update setup_nginx_certbot.sh

--- setup_nginx_certbot.sh
+++ setup_nginx_certbot.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 CERTBOT='yes'
-sudo apt install nginx 
+sudo apt install -y nginx 
 
-if [ $1 = "nocertbot" or $1 = "nc"]; then
+if [ $1 = "nocertbot" or $1 = "nc" ]; then
     CERTBOT='no'
 fi
 
 if [ $CERTBOT = "yes" ]; then
     sudo apt-get update &&\
-    sudo apt-get install software-properties-common &&\
+    sudo apt-get install -y software-properties-common &&\
     sudo add-apt-repository universe &&\
     sudo add-apt-repository ppa:certbot/certbot &&\
     sudo apt-get update &&\
-    sudo apt-get install certbot python-certbot-nginx &&\
+    sudo apt-get install -y certbot python-certbot-nginx &&\
 fi
