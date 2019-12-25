e70d1c03461ba7099e2edf9a1308f5bff6092129
Kun <quinnHugh@hotmail.com>
2019-12-25 15:20:22 +0800

    Update setup_anaconda.sh

--- setup_anaconda.sh
+++ setup_anaconda.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-5.3.1-Linux-x86_64.sh
+wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-2019.10-Linux-x86_64.sh
 
-chmod +x ./Anaconda3-5.3.1-Linux-x86_64.sh
+chmod +x ./Anaconda3-2019.10-Linux-x86_64.sh
 
-bash ./Anaconda3-5.3.1-Linux-x86_64.sh
+bash ./Anaconda3-2019.10-Linux-x86_64.sh
