21edfcca89a82c84b5c2182e7553caeae61bc430
kun <quinnhugh@hotmail.com>
2020-07-18 11:00:53 +0800

    update

--- nvidia-driver.sh
+++ nvidia-driver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo apt update && sudo apt install -y ubuntu-drivers-common &&\
-sudo ubuntu-drivers autoinstall && nvidia-smi 
+sudo ubuntu-drivers autoinstall && echo "nvidia driver install successed but you have to reboot for use"
 
