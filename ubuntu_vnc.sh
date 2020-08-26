1b216693db506bf16c4aaba12f1e804e93bb2970
kun <quinnhugh@hotmail.com>
2020-08-26 12:08:32 +0800

    feat: update a script to install vncserver in ubuntu
    The vnc installation in ubuntu is quiet complicated cause the gnome in ubuntu not so perfect, thus
    the vnc installation of ubuntu always get a gray windows. I found a way to config vncserver in
    ubuntu and it is stable through replace the gnome by xfce4.

---
+++ ubuntu_vnc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install -y xfce4 xfce4-goodies tightvncserver && mkdir ~/.vnc && cat << EOF | tee -a ~/.vnc/xstartup
+#!/bin/bash
+
+[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+startxfce4 &
+EOF
